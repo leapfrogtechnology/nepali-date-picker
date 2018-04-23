a84b0b8bf584a527fdb3c66144631aa209793217
SanishMaharjan <sanishmaharjan@lftechnology.com>
2018-04-23 10:52:06 +0545

    release version 2.0.0

--- src/jquery.nepaliDatePicker.js
+++ src/jquery.nepaliDatePicker.js
@@ -1,6 +1,6 @@
 /*
  * @fileOverview NepaliDatePicker - jQuery Plugin
- * @version 1.2.2
+ * @version 2.0.0
  *
  * @author Sanish Maharjan https://github.com/sanishmaharjan
  * @see https://github.com/sanishmaharjan/
